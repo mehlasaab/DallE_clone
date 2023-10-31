573023d574cf26be3c4d93f2f0cadb039ae1b177
mehlasaab <sanju.mehla3599@gmail.com>
2023-10-31 23:53:38 +0530

    SetUp the app

---
+++ client/src/pages/index.js
@@ -0,0 +1,9 @@
+import Home from "./Home";
+import CreatePost from "./CreatePost";
+
+export{
+    Home,
+    CreatePost
+}
+
+//This will allow us to import both home and createPost in one single line in App.jsx
