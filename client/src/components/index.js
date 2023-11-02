328e62a8edbfa80c083df3324021c21b893b9246
mehlasaab <sanju.mehla3599@gmail.com>
2023-11-02 12:54:16 +0530

    HomePage is built

---
+++ client/src/components/index.js
@@ -0,0 +1,11 @@
+import Card from "./Card";
+import FormField from "./FormField";
+import Loader from "./Loader";
+
+export{
+    Card,
+    FormField,
+    Loader
+}
+
+// To export all the 3 components as a single object in the HomePage
