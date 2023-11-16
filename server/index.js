284616bcb9cd743ec958e33448f88b98be909101
mehlasaab <sanju.mehla3599@gmail.com>
2023-11-17 03:53:35 +0530

    database is connected, mongoDB part is done

---
+++ server/index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+import connectDB from './mongodb/connect.js';
+
+dotenv.config(); //this will pull all the env var from .env file
+
+const app = express();
+
+// CORS is a security feature implemented by web browsers to restrict webpages from making requests to a different domain than the one that served the original web page. This is done to prevent potential security vulnerabilities, such as cross-site request forgery.
+// In a Node.js application, if your server is serving resources (like APIs or static files) and your client-side code is hosted on a different domain, you might encounter CORS restrictions. To allow cross-origin requests, you need to configure your Node.js server to include the appropriate CORS headers in its responses.
+app.use(cors()); //server will include the necessary headers in its responses to allow cross-origin requests from the specified domains
+
+app.use(express.json({ limit : '50mb' })); // json obj limit
+
+app.get('/',async (req,res) => {
+    res.send('Hello mf !')
+})
+
+const startServer = async () => {
+    try {
+        connectDB(process.env.MONGODB_URL);
+        app.listen(3000, () => console.log('Server has started on port http://localhost:3000'))
+    } catch (error) {
+        console.log(error);
+    }
+}
+startServer();
